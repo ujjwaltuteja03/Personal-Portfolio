df6b82756be41bb6441f65cd4185522af04d742d
ujjwal <ujjwal.tuteja03@gmail.com>
2026-08-24 15:39:44 +0530

    Updated a section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,8 +4,12 @@ export default function About() {
       <div className="max-w-2xl mx-auto px-6 p-8 rounded-lg text-center" style={{ fontFamily: 'Helvetica Neue, sans-serif' }}>
         {/* <h2 className="text-3xl font-bold mb-6 text-white">About</h2> */}
         <p className="text-lg text-gray-200  leading-relaxed mb-4">
-          A developer who loves exploring projects, skills and tech that intrigues me.
-          Recently, I've completed an ML internship at DRDO, working with quantum algorithms. Probably somewhere working on my dev skills or solving DSA problems using Python right now.
+          A developer who loves exploring projects, skills and tech that intrigues me.<br/>
+          I enjoy understanding complex problems and how things work under the hood. <br/>
+        </p>
+        <p className="text-lg text-gray-200  leading-relaxed mb-4">
+          Recently, I've completed an ML internship at DRDO, working with quantum algorithms. 
+          Probably somewhere improving my dev skills or solving DSA problems using Python right now.
         </p>
         <p className="text-lg text-gray-200 leading-relaxed">
           <strong>Stack:</strong> React, Express, Node.js, PostgreSQL 
